4ca86ddd79fd7da971ec9eb56539c4e484f2ca2e
4rjunc <arjunmundamani@gmail.com>
2025-04-07 17:15:11 +0530

    🏗️ Added Buy, Sell v1

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -8,7 +8,7 @@ contract Pool {
   using Math for uint256;
   using Math for uint32;
 
-  mapping(address => uint256) public balances;
+  mapping(address => uint256) public balances; // Should not be public, kept it public for testing
   uint256 totalSupply;
   uint32 slope;
 
@@ -17,23 +17,45 @@ contract Pool {
       slope = _slope;
   }
 
-  function buy() public payable  {
-   require(msg.value > 0, "Sent some ETH");
-   uint256 tokensToMint = calculateBuyReturn();
-    bool success;
-   //totalSupply += tokensToMint; // unable to use .add() from SafeMath
-   (success, totalSupply) = totalSupply.tryAdd(tokensToMint);
-   balances[msg.sender] = tokensToMint;
+  function sell(uint256 tokens) public {
+    totalSupply = totalSupply - tokens;
+    uint256 currentBalance = balances[msg.sender];
+    balances[msg.sender] = currentBalance - tokens;
+    
+    uint256 ethReturn = calculateSellReturn(tokens);
+    
+    payable(msg.sender).transfer(ethReturn);
   }
 
-  function calculateBuyReturn() public view returns(uint256) {
+  function buy() public payable  {
+   require(msg.value > 0, "Sent some ETH");
 
+   // calculate how number token can be brought by X amount of eth
+   uint256 tokensToMint = calculateBuyReturn(msg.value); 
+   //bool success;
+   //totalSupply += tokensToMint; // unable to use .add() from SafeMath
+   //(success, totalSupply) = totalSupply.tryAdd(tokensToMint);
+   totalSupply += tokensToMint;
+   uint256 currentBalance = balances[msg.sender];
+   balances[msg.sender] = currentBalance + tokensToMint;
+  }
+
+  function calculateSellReturn(uint256 tokens) public view returns (uint256){
+    uint256 currentPrice = calculateTokenPrice();
+    return  tokens / currentPrice;
+  }
+
+  function calculateBuyReturn(uint256 depositAmount) public view returns(uint256) {
+    uint256 currentPrice = calculateTokenPrice();
+    return (depositAmount * 1e18) / currentPrice;
   }
 
   function calculateTokenPrice() public view returns(uint256) {
-    uint256 temp;
-    bool success;
-    (success, temp) = totalSupply.tryMul(totalSupply);
+    uint256 temp = totalSupply * totalSupply;
+    //bool success;
+    //(success, temp) = totalSupply.tryMul(totalSupply);
+    // In demo it was return slope.mul(trmp); 
+    // find the differene between the arithmetic operations like *, .tryMul() and .mul()
     return  slope * temp;
   }
 }
--- test/Pool.js
+++ test/Pool.js
@@ -1,16 +1,36 @@
 const { ethers } = require("hardhat");
 
+
 describe("Pool", () => {
   it("should work", async () => {
 
     const [owner, otherAccount] = await ethers.getSigners();
     const Pool = await ethers.getContractFactory("Pool");
 
-    const initialSupply = 200;
+    const initialSupply = ethers.parseUnits("20", 8);
     const slope = 1;
     const pool = await Pool.deploy(initialSupply, slope);
-    const tokenPrice = await pool.calculateTokenPrice();
 
-    console.log("Token Price: ", tokenPrice)
+    const tokenPrice = await pool.calculateTokenPrice();
+    console.log("Token Price Before Buy: ", tokenPrice);
+
+
+    //Buying
+    await pool.buy({ value: ethers.parseEther("2.0") }); // buying for 2 ETH
+
+    const newTokenPrice = await pool.calculateTokenPrice();
+    console.log("Token Price After Buy: ", newTokenPrice);
+
+    const balance = await pool.balances(owner.address);
+    console.log("Balance After Buy: ", balance);
+
+    //Selling
+    await pool.sell(balance);
+    const newTokenPriceAfterSell = await pool.calculateTokenPrice();
+    console.log("Token Price After Sell: ", newTokenPriceAfterSell);
+
+    const balanceAfterSell = await pool.balances(owner.address);
+    console.log("Balance After Sell: ", balanceAfterSell);
+
   })
 })  
