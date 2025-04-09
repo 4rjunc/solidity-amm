a62fa9199a7fc792e072c06a8f8eca3c52535e33
4rjunc <arjunmundamani@gmail.com>
2025-04-09 23:30:32 +0530

    Refactored the contract

--- contracts/Pool.sol
+++ contracts/Pool.sol
@@ -2,57 +2,98 @@
 
 pragma solidity 0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/utils/math/Math.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
-contract Pool is ERC20 {
-  using Math for uint256;
-  using Math for uint32;
+contract BondingCurvePool is ERC20 {
+    using Math for uint256;
 
-  //mapping(address => uint256) public balances; // Should not be public, kept it public for testing
-  //uint256 totalSupply;
-  uint32 slope;
-
-  constructor(uint256 initialSupply, uint32 _slope) ERC20("Gold", "GLD"){
-      _mint(msg.sender, initialSupply);
-      slope = _slope;
-  }
-
-  function sell(uint256 tokens) public {
-    require(balanceOf(msg.sender) >= tokens);
-
-    uint256 ethReturn = calculateSellReturn(tokens);
-    require(ethReturn <= address(this).balance, "Contract is broke like you"); // Checks if the contract has enough ETH to sent back
+    uint256 public reserveBalance;
+    uint256 public reserveRatio; 
     
-    _burn(msg.sender, tokens); // _mint and _burn are just a mapping function to handle balance of tokens
+    event TokensPurchased(address indexed buyer, uint256 amountEth, uint256 amountTokens);
+    event TokensSold(address indexed seller, uint256 amountTokens, uint256 amountEth);
 
-    payable(msg.sender).transfer(ethReturn);
-  }
+    constructor(
+        string memory name,
+        string memory symbol,
+        uint256 _reserveRatio
+    ) ERC20(name, symbol) {
+        require(_reserveRatio > 0 && _reserveRatio <= 100, "Reserve ratio must be between 1-100");
+        reserveRatio = _reserveRatio;
+    }
 
-  function buy() public payable  {
-   require(msg.value > 0, "Sent some ETH");
+    function calculateCurrentPrice() public view returns (uint256) {
+        if (totalSupply() == 0) {
+            return 1e15; // Initial price of 0.001 ETH per token
+        }
+        
+        return (reserveBalance * 1e18) / (totalSupply() * reserveRatio / 100);
+    }
 
-   // calculate how number token can be brought by X amount of eth
-   uint256 tokensToMint = calculateBuyReturn(msg.value); 
-   _mint(msg.sender, tokensToMint);
+    // Calculate how many tokens will be minted for a given ETH amount
+    function calculateBuyReturn(uint256 ethAmount) public view returns (uint256) {
+        if (totalSupply() == 0) {
+            return ethAmount * 1e3; // Initial exchange rate
+        }
 
-  }
+        uint256 currentSupply = totalSupply();
+        uint256 currentReserve = reserveBalance;
+        
+        // Formula: supply * ((1 + deposit/reserve)^(reserveRatio/100) - 1)
+        // We simplify for small purchases: tokens = deposit * supply / (reserve * reserveRatio/100)
+        return (ethAmount * currentSupply) / (currentReserve * reserveRatio / 100);
+    }
 
-  function calculateSellReturn(uint256 tokens) public view returns (uint256){
-    uint256 currentPrice = calculateTokenPrice();
-    return  tokens / currentPrice;
-  }
+    // Calculate how much ETH will be returned for a given token amount
+    function calculateSellReturn(uint256 tokenAmount) public view returns (uint256) {
+        require(totalSupply() > 0, "No tokens in circulation");
+        require(tokenAmount <= totalSupply(), "Not enough tokens in circulation");
+        
+        uint256 currentSupply = totalSupply();
+        uint256 currentReserve = reserveBalance;
+        
+        // Formula: reserve * (1 - (1 - tokenAmount/supply)^(100/reserveRatio))
+        // We simplify for small sales: eth = tokens * reserve * reserveRatio/100 / supply
+        return (tokenAmount * currentReserve * reserveRatio / 100) / currentSupply;
+    }
 
-  function calculateBuyReturn(uint256 depositAmount) public view returns(uint256) {
-    uint256 currentPrice = calculateTokenPrice();
-    return (depositAmount * 1e18) / currentPrice;
-  }
+    // Buy tokens with ETH
+    function buy() public payable {
+        require(msg.value > 0, "Must send ETH to buy tokens");
+        
+        uint256 tokensToMint = calculateBuyReturn(msg.value);
+        require(tokensToMint > 0, "Not enough ETH sent");
+        
+        reserveBalance += msg.value;
+        _mint(msg.sender, tokensToMint);
+        
+        emit TokensPurchased(msg.sender, msg.value, tokensToMint);
+    }
 
-  function calculateTokenPrice() public view returns(uint256) {
-    uint256 supply = totalSupply();
-    uint256 temp = supply * supply;
-    return  slope * temp;
-  }
-  
-  receive() external payable{}
+    // Sell tokens to get ETH back
+    function sell(uint256 tokenAmount) public {
+        require(tokenAmount > 0, "Must sell more than 0 tokens");
+        require(balanceOf(msg.sender) >= tokenAmount, "Not enough tokens to sell");
+        
+        uint256 ethToReturn = calculateSellReturn(tokenAmount);
+        require(ethToReturn > 0, "Not enough tokens to receive ETH");
+        require(ethToReturn <= address(this).balance, "Contract has insufficient ETH");
+        
+        _burn(msg.sender, tokenAmount);
+        reserveBalance -= ethToReturn;
+        payable(msg.sender).transfer(ethToReturn);
+        
+        emit TokensSold(msg.sender, tokenAmount, ethToReturn);
+    }
+    
+    // Fallback function to handle ETH transfers
+    receive() external payable {
+        // Auto-buy tokens when ETH is sent to the contract
+        if (msg.sender != address(0)) {
+            buy();
+        } else {
+            reserveBalance += msg.value;
+        }
+    }
 }
