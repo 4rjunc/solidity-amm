50f63c84f11a7d0b34105a2c182b099d73488a2e
4rjunc <arjunmundamani@gmail.com>
2025-04-09 10:59:49 +0530

    👾 Refactored contract to show the balances as ETH

--- test/Pool.js
+++ test/Pool.js
@@ -7,12 +7,21 @@ describe("Pool", () => {
     const [owner, otherAccount] = await ethers.getSigners();
     const Pool = await ethers.getContractFactory("Pool");
 
+
     const initialSupply = ethers.parseUnits("20", 8);
     const slope = 1;
     const pool = await Pool.deploy(initialSupply, slope);
 
+    await owner.sendTransaction({
+      to: pool.getAddress(),
+      value: ethers.parseEther("1000.0")
+    })
+
+    const contractBalance = await ethers.provider.getBalance(pool.getAddress());
+    console.log("contractBalance: ", ethers.formatEther(contractBalance), " ETH")
+
     const tokenPrice = await pool.calculateTokenPrice();
-    console.log("Token Price Before Buy: ", tokenPrice);
+    console.log("Token Price Before Buy: ", ethers.formatEther(tokenPrice), " ETH per Token")
 
 
     console.log("\n-----BUYING------");
@@ -20,19 +29,23 @@ describe("Pool", () => {
     await pool.buy({ value: ethers.parseEther("2.0") }); // buying for 2 ETH
 
     const newTokenPrice = await pool.calculateTokenPrice();
-    console.log("Token Price After Buy: ", newTokenPrice);
+    console.log("Token Price After Buy: ", ethers.formatEther(newTokenPrice), " ETH per Token")
 
     const balance = await pool.balances(owner.address);
-    console.log("Balance After Buy: ", balance);
+    console.log("Balance After Buy: ", ethers.formatUnits(balance, 0), " tokens");
+
+
+    const contractBalanceAfterBuy = await ethers.provider.getBalance(pool.getAddress());
+    console.log("contractBalance After Buy: ", ethers.formatEther(contractBalanceAfterBuy), " ETH")
 
     console.log("\n-----SELLING------");
     //Selling
     await pool.sell(balance);
     const newTokenPriceAfterSell = await pool.calculateTokenPrice();
-    console.log("Token Price After Sell: ", newTokenPriceAfterSell);
+    console.log("Token Price After Sell: ", ethers.formatEther(newTokenPriceAfterSell), " ETH")
 
     const balanceAfterSell = await pool.balances(owner.address);
-    console.log("Balance After Sell: ", balanceAfterSell);
+    console.log("Balance After Sell: ", ethers.formatUnits(balanceAfterSell), " tokens")
 
   })
 })  
